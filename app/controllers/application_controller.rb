27910acbb393e7fde24f47910d88d4e268b6c680
Adnan Ahmad <adnansahmad@outlook.com>
2024-12-13 22:24:06 +0500

    setup devise and make it working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    before_action :set_application_name 
+    protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def set_application_name
+        @app_name = "Task Reminder"
+    end
+
+    protected
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password)}
+    end
+
 end
