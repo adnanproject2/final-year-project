57c014947e6e2bd0474c43979f18c0f33284a96e
Adnan Ahmad <adnansahmad@outlook.com>
2025-03-04 14:55:38 +0500

    add loader

--- test/mailers/previews/task_notify_mailer_preview.rb
+++ test/mailers/previews/task_notify_mailer_preview.rb
@@ -1,6 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/task_notify_mailer
 class TaskNotifyMailerPreview < ActionMailer::Preview
     def task_reminder()
-        TaskNotifyMailer.task_reminder("ff")
+        TaskNotifyMailer.task_reminder(Task.last)
+    end 
+
+    def daily_reminder()
+        TaskNotifyMailer.daily_reminder(Account.last, Account.last.tasks)
     end
 end
